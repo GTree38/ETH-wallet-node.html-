a23b190c42f2c04e4b77d5ca024ccd62dabd32e2
GTree38 <loveme941210@gmail.com>
2022-06-21 17:15:03 +0900

    erc20 솔리디티 파일 추가

---
+++ erc20token.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.16;
+
+contract MinimumViableToken {
+    mapping(address => uint256) public balanceOf;
+    
+    constructor() public {}
+    
+    function MinimumViableTokens(uint256 initialSupply) public {
+        balanceOf[msg.sender] = initialSupply;
+    }
+    
+    function transfer(address _to, uint256 _value) public {
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+    }
+}
